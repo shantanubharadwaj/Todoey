f5ca02fe7079dc3089fb1fb4de735778086b45c9
Shantanu <shantanudutta.bharadwaj@gmail.com>
2018-08-21 22:11:53 +0530

    Todo list complete with datasource and delegate methods

---
+++ Todoey/Controllers/TodoListViewController.swift
@@ -0,0 +1,101 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Shantanu Dutta on 19/08/18.
+//  Copyright © 2018 Shantanu Dutta. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+//    let dataFilePath: URL? = docuPath.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let newItem = Item(title: "Find Mike")
+        itemArray.append(newItem)
+        let newItem2 = Item(title: "Buy Eggos")
+        itemArray.append(newItem2)
+        let newItem3 = Item(title: "Destroy Demogorgon")
+        itemArray.append(newItem3)
+        
+        loadItems()
+    }
+    
+    // MARK: Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            if let text = textField.text, !text.isEmpty {
+                let newItem = Item(title: text)
+                self.itemArray.append(newItem)
+                self.saveItems()
+            }
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch let error {
+            print("Error saving data : \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch let error {
+                print("Error saving data : \(error)")
+            }
+        }
+        
+        self.tableView.reloadData()
+    }
+}
+
+extension TodoListViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }else{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
